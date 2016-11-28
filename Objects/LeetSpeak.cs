86fbdf0d2b1c1c68ce893e02f9a03f615e3bc54c
Dave Denlow and Ryan Peterson <dave+ryan@>
2016-11-28 10:37:49 -0800

    add function that changes string to array

--- Objects/LeetSpeak.cs
+++ Objects/LeetSpeak.cs
@@ -7,6 +7,7 @@ namespace LeetSpeak
   {
     private string _userWord;
     private string _leetWord;
+    private char[] _userWordArray;
 
 
     public bool CheckUserWord()
@@ -21,10 +22,38 @@ namespace LeetSpeak
       }
     }
 
+    public bool CheckArray()
+    {
+      if(_userWordArray.Length == 0)
+      {
+        return false;
+      }
+      else
+      {
+        return true;
+      }
+    }
+
+    public char[] WordToArray(string word)
+    {
+      _userWordArray = word.ToCharArray();
+      return _userWordArray;
+    }
+
+
+    public string GetUserWord()
+    {
+      return _userWord;
+    }
+
     public void SetUserWord()
     {
       _userWord = Console.ReadLine();
     }
 
+    public char[] GetUserArray()
+    {
+      return _userWordArray;
+    }
   }
 }
--- Tests/LeetSpeakTests.cs
+++ Tests/LeetSpeakTests.cs
@@ -11,5 +11,15 @@ namespace LeetSpeak
       leetWord.SetUserWord();
       Assert.Equal(true, leetWord.CheckUserWord());
     }
+
+    [Fact]
+    public void checkArray()
+    {
+      LeetSpeak leetWord = new LeetSpeak();
+      leetWord.SetUserWord();
+      leetWord.WordToArray(leetWord.GetUserWord());
+
+      Assert.Equal(true, leetWord.CheckArray());
+    }
   }
 }
